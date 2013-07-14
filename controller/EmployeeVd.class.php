554ef42629cb9e8c27111e9a3607667905fd3c8a
guilhermecaldas <guilhermecaldas@yandex.com>
2013-07-14 03:30:17 -0300

    Create Validation

--- controller/EmployeeVd.class.php
+++ controller/EmployeeVd.class.php
@@ -1,72 +1,48 @@
 <?php
 
-class ReservaVd {
+class EmployeeVd {
 
-    private static $nroReserva;
-    private static $valor;
-    private static $codCliente;
-    private static $dias;
-    private static $extras = 0;
-    private static $desconto = 0;
+    private static $id;
+    private static $name;
+    private static $user_name;
+    private static $password;
+    private static $picture;
 
     public static function validation() {
 
-        if (!is_numeric($_POST["txNroReserva"]))
-            throw new Exception("nro Reserva inválido");
+        if (!is_numeric($_POST["txName"]))
+            throw new Exception("Nome inválido"); //invalid employee name
 
-        self::$nroReserva = $_POST["txNroReserva"];
+        self::$name = $_POST["txName"];
 
-        if (empty($_POST["lsTipoQuarto"]) ||
-                ((int) $_POST["lsTipoQuarto"]) == 0)
-            throw new Exception("escolha tipo quarto");
+        if (!ctype_alnum($_POST["txUserName"]))
+            throw new Exception("Nome de usuário inválido"); //invalid username
 
-        if (isset($_POST["cbCamareira"]))
-            self::$extras+= 20;
-        if (isset($_POST["cbFrigobar"]))
-            self::$extras+= 60;
+        self::$user_name = $_POST["txUserName"];
 
-        if (!is_numeric($_POST["txDias"]) ||
-                ((int) $_POST["txDias"]) <= 0)
-            throw new Exception("Dias inválido");
-        if (!empty($_POST["txDesconto"])) {
-            if (!is_numeric($_POST["txDesconto"]) ||
-                    ((int) $_POST["txDesconto"]) <= 0 ||
-                    ((int) $_POST["txDesconto"]) > 30)
-                throw new Exception("desconto inválido");
-
-            self::$desconto = $_POST["txDesconto"];
-        }
-        if (!is_numeric($_POST["txCodCliente"]))
-            throw new Exception("Cód cliente inválido");
-
-        //self é this para os atributos estaticos.	
-        self::$dias = $_POST["txDias"];
-        self::$codCliente = $_POST["txCodCliente"];
-        self::$valor = $_POST["lsTipoQuarto"];
+        self::$id = $_POST["numId"];
+        self::$password = $_POST["txPassword"];
+        self::$picture = $_POST["txPicture"];
     }
 
-    public static function getNroReserva() {
-        return self::$nroReserva;
+    public static function getId() {
+        return self::$id;
     }
 
-    public static function getCodCliente() {
-        return self::$codCliente;
+    public static function getNome() {
+        return self::$name;
     }
 
-    public static function getDias() {
-        return self::$dias;
+    public static function getUserName() {
+        return self::$user_name;
     }
 
-    public static function getExtras() {
-        return self::$extras;
+    public static function getPassword() {
+        return self::$password;
     }
 
-    public static function getDesconto() {
-        return self::$desconto;
-    }
-
-    public static function getValor() {
-        return self::$valor;
+    public static function getPicture() {
+        return self::$picture;
     }
 
 }
