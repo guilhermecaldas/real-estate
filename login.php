e6cf9bb433a8500fc011c3ac2e540a56267747c6
Nikita <nik.pr2012@yandex.ru>
2013-07-18 11:26:03 +0400

    Add to the login screen

--- login.php
+++ login.php
@@ -1,12 +1,62 @@
 <html>
     <?php
-    include './head.php';
+    include './head.php';   
     ?>
+    <?php
+session_start();
+include "model/connection.class.php";
 
-    <body>
-        <h1>Here is the login screen</h1>
-    </body>
+$login = $admin_login;
+$password = $admin_password;
+if (isset($_SESSION['admin'])) { $print_form = "already_login"; }
 
+if ($print_form != "already_login")
+{
+   if (!@$_POST)
+{
+      $print_form = 1;
+}
+
+   else
+
+{
+    $posted_admin_login = $_POST['login'];
+	$posted_admin_password =  $_POST['password'];
+	
+	
+	if ($posted_admin_login == $login && $posted_admin_password == $password)	
+	{	 
+		 $_SESSION['admin'] =  $posted_admin_login;    	          
+		 $print_form = 0;
+        }
+        else
+        {
+	    $msg = "You entered an incorrect username or password.<br>Hands will be amputated!";
+		 $print_form = 1;
+        }
+}
+}
+   if ($print_form != 1 or $print_form == "already_login")
+   {  
+  header("Location: main_screen.php");  
+   }
+   else
+   {
+	?>		
+<center>
+   <div class="container">       
+   <span class="label label-important"><?=@$msg?></span>   
+   <form action="<?=$_SERVER["PHP_SELF"] ?>" method="POST" name="admin_login">     
+   <h2 class="form-signin-heading">Log in to the administrator</h2> 
+	<input type="text" name="login" value=""><br/>
+	<input type="password"  name="password" value=""><br/>	
+	<button class="btn btn-large btn-primary" type="submit">Log in</button>			
+   </form>
+   </div>
+   </center>
+   <?php 
+   }   
+?>
     <?php
     include './foot.php';
     ?>
