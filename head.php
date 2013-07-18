46a64012e0e45f4d37a9cc6e5595464f06d8f253
Nikita <nik.pr2012@yandex.ru>
2013-07-18 11:27:40 +0400

    Add to the login screen

--- head.php
+++ head.php
@@ -1,5 +1,7 @@
 <head>
     <title>Imobiliaria Carneiro</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="layout.css">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico"> 
+    <style type="text/css">body{padding-top:40px;padding-bottom:40px;background-color:#f5f5f5;background-repeat:repeat;}</style>
 </head>
